d9440bcec1d3ec4c9467d898c8ca063f2afcfe0f
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2023-11-07 13:24:11 -0300

    first steps

---
+++ src/categories/ExpensesCategory.java
@@ -0,0 +1,17 @@
+package categories;
+
+public class ExpensesCategory {
+    private String name;
+
+    public ExpensesCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/categories/IncomesCategory.java
@@ -0,0 +1,17 @@
+package categories;
+
+public class IncomesCategory {
+    private String name;
+
+    public IncomesCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
